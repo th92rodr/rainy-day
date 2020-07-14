4772755b2604081470dd9b22dce01062c1b16781
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-07-14 11:13:49 -0300

    API: update get-current-weather endpoint tests

--- api/src/__tests__/Weather.test.ts
+++ api/src/__tests__/Weather.test.ts
@@ -3,9 +3,45 @@ import request from 'supertest';
 import app from '../app';
 
 describe('Weather', () => {
-  it('should be able to get the current weather', async () => {
-    const cityName = 'London';
-    const response = await request(app).get(`/weather/${cityName}/current`);
+  it('should be able to get the current weather by address', async () => {
+    const country = 'BR';
+    const state = 'SP';
+    const city = 'Campinas';
+
+    const response = await request(app).get(
+      `/weather/address?country=${country}&state=${state}&city=${city}`,
+    );
+
+    expect(response.status).toEqual(200);
+    expect(response.body).toMatchObject(
+      expect.objectContaining({
+        cityName: expect.any(String),
+        country: expect.any(String),
+        coordinates: expect.objectContaining({
+          longitude: expect.any(Number),
+          latitude: expect.any(Number),
+        }),
+        temperature: expect.any(Number),
+        feelsLike: expect.any(Number),
+        temperatureMin: expect.any(Number),
+        temperatureMax: expect.any(Number),
+        pressure: expect.any(Number),
+        humidity: expect.any(Number),
+        windSpeed: expect.any(Number),
+        cloudiness: expect.any(Number),
+        sunrise: expect.any(Number),
+        sunset: expect.any(Number),
+      }),
+    );
+  });
+
+  it('should be able to get the current weather by coordinates', async () => {
+    const latitude = -22.8920562;
+    const longitude = -47.2083213;
+
+    const response = await request(app).get(
+      `/weather/coords?latitude=${latitude}&longitude=${longitude}`,
+    );
 
     expect(response.status).toEqual(200);
     expect(response.body).toMatchObject(
@@ -31,8 +67,13 @@ describe('Weather', () => {
   });
 
   it('should not be able to get the current weather of an invalid city', async () => {
-    const cityName = 'Fake Name';
-    const response = await request(app).get(`/weather/${cityName}/current`);
+    const country = 'Fake';
+    const state = 'Fake';
+    const city = 'Fake Name';
+
+    const response = await request(app).get(
+      `/weather/address?country=${country}&state=${state}&city=${city}`,
+    );
 
     expect(response.status).toEqual(400);
   });
