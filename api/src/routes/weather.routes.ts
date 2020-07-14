7517cbfa47e38f53a416cbcb20fcafa28925d312
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-07-14 10:56:42 -0300

    API: update weather routes and add celebrate validation

--- api/src/routes/weather.routes.ts
+++ api/src/routes/weather.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
 
+import celebrateConfig from '../config/celebrate';
 import WeatherController from '../controllers/weather.controller';
+import { forecastValidationSchema } from '../middlewares/validationSchemas';
 
 const weatherRoutes = Router();
 
 const weatherController = new WeatherController();
 
-weatherRoutes.get('/:city/current', weatherController.getCurrentWeather);
+weatherRoutes.get(
+  '/:search',
+  celebrate(forecastValidationSchema, celebrateConfig),
+  weatherController.getCurrentWeather,
+);
+weatherRoutes.get(
+  '/forecast/:search',
+  celebrate(forecastValidationSchema, celebrateConfig),
+  weatherController.getForecast,
+);
 
 export default weatherRoutes;
