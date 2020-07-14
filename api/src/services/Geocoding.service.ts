0a54af7823108293971620d351f6b5541e06f80b
th92rodr <thiago.clrodrigues@gmail.com>
2020-07-14 10:36:14 -0300

    create geocoding service

---
+++ api/src/services/Geocoding.service.ts
@@ -0,0 +1,44 @@
+import nodeGeocoder from 'node-geocoder';
+
+interface Response {
+  latitude: number;
+  longitude: number;
+}
+
+interface GeoCoderResponse {
+  data: [
+    {
+      latitude: number;
+      longitude: number;
+      formattedAddress: string;
+      country: string;
+      city: string;
+      state: string;
+      zipcode?: number;
+      streetName?: string;
+      streetNumber?: number;
+      countryCode: string;
+      neighbourhood?: string;
+      provider: string;
+    },
+  ];
+}
+
+export default class GeocodingService {
+  public async execute(address: string): Promise<Response> {
+    const geocoder = nodeGeocoder({
+      provider: 'openstreetmap',
+    });
+
+    const geoCoderResponse = await geocoder.geocode(address);
+
+    return this.prepareResponse(geoCoderResponse);
+  }
+
+  private prepareResponse(data: nodeGeocoder.Entry[]): Response {
+    return {
+      latitude: data[0].latitude as number,
+      longitude: data[0].longitude as number,
+    };
+  }
+}
