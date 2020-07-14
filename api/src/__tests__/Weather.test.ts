ac36db6bc828cd2aa69b23b90fec8acdd3c8a456
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-07-14 11:16:16 -0300

    API: add get-forecast endpoint tests

--- api/src/__tests__/Weather.test.ts
+++ api/src/__tests__/Weather.test.ts
@@ -77,4 +77,45 @@ describe('Weather', () => {
 
     expect(response.status).toEqual(400);
   });
+
+  it('should be able to get the forecast by address', async () => {
+    const country = 'BR';
+    const state = 'SP';
+    const city = 'Campinas';
+    const address = 'Dr. Heitor Penteado';
+    const type = 'Avenida';
+    const number = 1671;
+
+    const response = await request(app).get(
+      `/weather/forecast/address?country=${country}&state=${state}&city=${city}&address=${address}&type=${type}&number=${number}`,
+    );
+
+    expect(response.status).toEqual(200);
+  });
+
+  it('should be able to get the forecast by coordinates', async () => {
+    const latitude = -22.8920562;
+    const longitude = -47.2083213;
+
+    const response = await request(app).get(
+      `/weather/forecast/coords?latitude=${latitude}&longitude=${longitude}`,
+    );
+
+    expect(response.status).toEqual(200);
+  });
+
+  it('should not be able to get the forecast of an invalid city', async () => {
+    const country = 'Fake';
+    const state = 'Fake';
+    const city = 'Fake City';
+    const address = 'Fake Address';
+    const type = 'Fake Street';
+    const number = 0;
+
+    const response = await request(app).get(
+      `/weather/forecast/address?country=${country}&state=${state}&city=${city}&address=${address}&type=${type}&number=${number}`,
+    );
+
+    expect(response.status).toEqual(400);
+  });
 });
