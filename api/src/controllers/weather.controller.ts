18e1986dc676decef8cb09b2d1d707c8f4b81d1a
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-07-14 10:59:04 -0300

    API: update get-current-weather and create get-forecast methods in weather controller

--- api/src/controllers/weather.controller.ts
+++ api/src/controllers/weather.controller.ts
@@ -1,17 +1,53 @@
 import { Request, Response } from 'express';
 
+import GetForecastService from '../services/GetForecast.service';
 import GetCurrentWeatherService from '../services/GetCurrentWeather.service';
 
 export default class WeatherController {
   public async getCurrentWeather(request: Request, response: Response) {
-    const { city } = request.params;
+    const { search } = request.params;
+
+    const getCurrentWeatherService = new GetCurrentWeatherService();
 
     try {
-      const getCurrentWeatherService = new GetCurrentWeatherService();
+      const currentWeather = await getCurrentWeatherService.execute({
+        search,
 
-      const currentWeather = await getCurrentWeatherService.execute({ city });
+        city: request.query.city as string | undefined,
+        state: request.query.state as string | undefined,
+        country: request.query.country as string | undefined,
+
+        latitude: request.query.latitude as number | undefined,
+        longitude: request.query.longitude as number | undefined,
+      });
 
       return response.json(currentWeather);
+    } catch (error) {
+      return response.status(400).json({ message: error.message });
+    }
+  }
+
+  public async getForecast(request: Request, response: Response) {
+    const { search } = request.params;
+
+    const getForecastService = new GetForecastService();
+
+    try {
+      const forecast = await getForecastService.execute({
+        search,
+
+        city: request.query.city as string | undefined,
+        state: request.query.state as string | undefined,
+        country: request.query.country as string | undefined,
+        address: request.query.address as string | undefined,
+        type: request.query.type as string | undefined,
+        number: request.query.number as number | undefined,
+
+        latitude: request.query.latitude as number | undefined,
+        longitude: request.query.longitude as number | undefined,
+      });
+
+      return response.json(forecast);
     } catch (error) {
       return response.status(400).json({ message: error.message });
     }
